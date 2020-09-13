f564fb83db52584665ecde1c0dc857d8986d84e9
frenki1231 <70661525+frenki1231@users.noreply.github.com>
2020-09-13 20:57:13 +0300

    fix: add the solution of the problem

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let count=0;
+  for (let mus of arr){
+    count+=mus.filter(item=>item=="^^").length;
+  }
+  return count;
 };
